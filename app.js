f03548412576dfc5840263468bc352a8b85995f9
unknown <areczcki>
2019-08-25 00:12:10 -0300

    Trabalhando com listas

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var app = new Vue({
     el: "#app",
     data: {
-        title: "Contas a pagar"
+        title: "Contas a pagar",
+        menus: [
+            {id:"0", name:"Listar Contas"},
+            {id:"1", name:"Adicionar Conta"}
+        ],
+        bills: [
+            {date_due: "10/08/2019", name: "Conta de Luz", value:"145.00", done:"1"},
+            {date_due: "10/08/2019", name: "Condominio", value:"230.00", done:"0"},
+            {date_due: "10/08/2019", name: "Cartão de Crédito", value:"400.00", done:"0"},
+            {date_due: "10/08/2019", name: "Internet", value:"105.00", done:"0"},
+            {date_due: "10/08/2019", name: "Gasolina", value:"600.00", done:"0"}
+        ],
     },
 });
